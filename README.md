b06e531eaa33b138ac4a5346f4f2b7be7a4f24e5
freewind <nowindlee@gmail.com>
2018-10-06 11:36:07 +0800

    demo

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-TypeScript Hello World Demo
-============================
+TypeScript Feathersjs Express Rest Demo
+=======================================
 
 ```
 npm install
 npm run demo
 ```
 
-It will print `Hello, TypeScript!`
+Then visit http://localhost:3000/messages and all kinds of rest requests of it
+
+```
+brew install httpie
+http http://localhost:3000/messages
+http POST http://localhost:3000/messages text=hello
+http PUT http://localhost:3000/messages/0 text=hello000
+http DELETE http://localhost:3000/messages/0
+```
--- hello.ts
+++ hello.ts
@@ -1,3 +1,23 @@
-import {capitalize} from 'lodash';
+import feathers from '@feathersjs/feathers';
+import express from '@feathersjs/express';
+import Messages from './Messages';
 
-console.log("Hello " + capitalize("typescript") + "!");
+const app = express(feathers());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.configure(express.rest());
+
+app.use(express.errorHandler());
+
+const messages = new Messages();
+
+app.use('messages', messages);
+
+app.listen(3000, async () => {
+  await messages.create({text: 'Hello'});
+  await messages.create({text: 'World'});
+  console.log('listen on http://localhost:3000')
+});
+
